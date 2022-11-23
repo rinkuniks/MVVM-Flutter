b5363887b37fbb8ff864aad3ca87c33b282d910c
Nikhil Kumar Singh <rinkuniks@gmail.com>
2022-11-23 18:50:29 +0530

    shimmer effect

--- mvvm_flutter/lib/view/home_screen.dart
+++ mvvm_flutter/lib/view/home_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutter/utils/routes/routes_names.dart';
-import 'package:mvvm_flutter/utils/utils.dart';
 import 'package:mvvm_flutter/view_model/home_view_model.dart';
 import 'package:mvvm_flutter/view_model/user_view_model.dart';
 import 'package:provider/provider.dart';
-
 import '../data/response/status.dart';
+import '../res/shimmer_widget.dart';
+import '../utils/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -29,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
         centerTitle: true,
         actions: [
           InkWell(
@@ -46,50 +46,63 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Consumer<HomeViewModel>(builder: (context, value, _) {
           switch (value.moviesList.status) {
             case Status.LOADING:
-              return Center(child: CircularProgressIndicator());
+              return ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return shimmerEffect();
+                  });
             case Status.ERROR:
               return Center(child: Text(value.moviesList.message.toString()));
             case Status.COMPLETED:
               return ListView.builder(
                   itemCount: value.moviesList.data!.movies!.length,
                   itemBuilder: (context, index) {
-                    return Card(
-                        child: ListTile(
-                      title: Text(value.moviesList.data!.movies![index].title
-                          .toString()),
-                      subtitle: Text(value.moviesList.data!.movies![index].year
-                          .toString()),
-                      leading: Image.network(
-                        value.moviesList.data!.movies![index].posterurl
-                            .toString(),
-                        errorBuilder: (context, error, stack) {
-                          return Icon(
-                            Icons.error,
-                            color: Colors.red,
-                          );
-                        },
-                        height: 40,
-                        width: 40,
-                        fit: BoxFit.cover,
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(Utils.avgRating(value
-                                  .moviesList.data!.movies![index].ratings!)
-                              .toString()),
-                          Icon(
-                            Icons.star,
-                            color: Colors.yellow,
-                          )
-                        ],
-                      ),
-                    ));
+                    return homePageView(value, index);
+                    // return shimmerEffect();
                   });
           }
           return Container();
         }),
       ),
     );
+  }
+
+  Widget shimmerEffect() => const ListTile(
+      title: ShimmerWidget.rectangular(height: 16),
+      subtitle: ShimmerWidget.rectangular(height: 14),
+      leading: ShimmerWidget.circular(
+        height: 64,
+        width: 64,
+      ));
+
+  Widget homePageView(HomeViewModel value, int index) {
+    return Card(
+        child: ListTile(
+      title: Text(value.moviesList.data!.movies![index].title.toString()),
+      subtitle: Text(value.moviesList.data!.movies![index].year.toString()),
+      leading: Image.network(
+        value.moviesList.data!.movies![index].posterurl.toString(),
+        errorBuilder: (context, error, stack) {
+          return const Icon(
+            Icons.error,
+            color: Colors.red,
+          );
+        },
+        height: 40,
+        width: 40,
+        fit: BoxFit.cover,
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(Utils.avgRating(value.moviesList.data!.movies![index].ratings!)
+              .toString()),
+          const Icon(
+            Icons.star,
+            color: Colors.yellow,
+          )
+        ],
+      ),
+    ));
   }
 }
